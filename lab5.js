d4923fa03d3721e25cd8444452cde14cddaaad19
rathes <krathesa@gmail.com>
2023-11-10 10:19:50 -0500

    add lab5.html

---
+++ lab5.js
@@ -0,0 +1,8 @@
+class CenterHtml extends HTMLElement{
+  connectedCallback(){
+    this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>;=`;
+
+  }
+
+}
+customerElements.define("x-center", CenterHtml);
